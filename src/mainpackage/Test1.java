3b5614f83c5c5263363dfdf715f2a4d2e6349ee7
HOME <HOME@HOME-PC>
2022-05-01 19:59:22 +0530

    Second commit

---
+++ src/mainpackage/Test1.java
@@ -0,0 +1,18 @@
+package mainpackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String fname = "dinesh";
+		if(fname=="dinesh") {
+			System.out.println("male");
+		}
+		else {
+			System.out.println("fmale");
+		}
+
+	}
+
+}
